fb042594096b3c7a8bbe6253f1c5ef6d24503eb9
N Nishanth Mony <54764054+nishanthmony@users.noreply.github.com>
2023-10-09 19:57:10 +0530

    span is used in cocurricular activities

--- src/components/cocurricular.js
+++ src/components/cocurricular.js
@@ -6,30 +6,30 @@ const Cocurricular = () => {
       <div className='container'>
         <h2>Activities</h2>
         <span className='line'></span>
-      <div className='content'>
-        <div className='card'>
-          <h3>Co-Curricular</h3>
-          <p><ul style={{listStyleType: 'number'}}>
-            <li>Photoshop</li>
-            <li>Digital Sketch</li>
-            <li>Pencil Sketch</li>
-          </ul></p>
+        <div className='content'>
+          <div className='card'>
+            <span><h3>Co-Curricular</h3></span>
+            <p><ul style={{listStyleType: 'number'}}>
+              <li>Photoshop</li>
+              <li>Digital Sketch</li>
+              <li>Pencil Sketch</li>
+            </ul></p>
+          </div>
+          <div className='card'>
+            <span><h3>Sports</h3></span>
+            <p><ul style={{listStyleType: 'number'}}>
+              <li>Badminton</li>
+              <li>Cricket</li>
+            </ul></p>
+          </div>
+          <div className='card'>
+            <span><h3>Extra curricular Activities</h3></span>
+            <p><ul style={{listStyleType: 'number'}}>
+              <li>karate</li>
+              <li>silambam</li>
+            </ul></p>
+          </div>
         </div>
-        <div className='card'>
-          <h3>Sports</h3>
-          <p><ul style={{listStyleType: 'number'}}>
-            <li>Badminton</li>
-            <li>Cricket</li>
-          </ul></p>
-        </div>
-        <div className='card'>
-          <h3>Extra curricular Activities</h3>
-          <p><ul style={{listStyleType: 'number'}}>
-            <li>karate</li>
-            <li>silambam</li>
-          </ul></p>
-        </div>
-      </div>
       </div>
     </div>
     </>
